32af5eb9de5551d83bb88825b491dae699f503f4
Smileyrasmus-UCN <1086243@ucn.dk>
2021-11-02 11:42:15 +0100

    Can get all people over api

--- FABS_WPF_Client/MainWindow.xaml.cs
+++ FABS_WPF_Client/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FABS_WPF_Client.Model;
+using RestSharp;
 
 namespace FABS_WPF_Client
 {
@@ -20,14 +23,32 @@ namespace FABS_WPF_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IRestClient _client = new RestClient("https://localhost:44337/api");
+
         public MainWindow()
         {
             InitializeComponent();
+            //RefreshList();
         }
 
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            var request = new RestRequest("people/", RestSharp.DataFormat.Json);
+            //var response = Client.Execute<PeopleList>(request);
+            var response = _client.Execute(request);
+            List<Person> peopleList = JsonSerializer.Deserialize<List<Person>>(response.Content);
+            //return response.Data.results;
+            ListOfPeople.ItemsSource = peopleList;
         }
     }
 }
---
+++ FABS_WPF_Client/Model/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FABS_WPF_Client.Model
+{
+    class Person
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+        [JsonPropertyName("telephoneNumber")]
+        public string TelephoneNumber { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(int id, string firstName, string lastName, string telephoneNumber)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            TelephoneNumber = telephoneNumber;
+        }
+    }
+}
