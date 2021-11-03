148306c7e43753e718403830a41e386618fcbd61
PHahn <1081439@ucn.dk>
2021-11-03 16:44:21 +0100

    Commit af mvc spike

---
+++ FABS_MVC/FABS_MVC/Controllers/PeopleController.cs
@@ -0,0 +1,97 @@
+﻿using FABS_MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FABS_MVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private IRestClient _client = new RestClient("https://localhost:44337/api");
+
+        // GET: PeopleController
+        public ActionResult Index()
+        {
+            var request = new RestRequest("people/", RestSharp.DataFormat.Json);
+            var response = _client.Execute(request);
+            List<Person> peopleList = JsonSerializer.Deserialize<List<Person>>(response.Content);
+
+            ViewData["people"] = peopleList;
+            return View();
+        }
+
+        // GET: PeopleController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PeopleController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PeopleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PeopleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PeopleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PeopleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PeopleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FABS_MVC/FABS_MVC/Models/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FABS_MVC.Models
+{
+    public class Person
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+        [JsonPropertyName("telephoneNumber")]
+        public string TelephoneNumber { get; set; }
+        [JsonPropertyName("adressesId")]
+        public int AdressesId { get; set; }
+        [JsonPropertyName("loginsId")]
+        public int LoginsId { get; set; }
+        [JsonPropertyName("isAdmin")]
+        public bool IsAdmin { get; set; }
+
+
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName, string telephoneNumber, int adressesId, int loginsId, bool isAdmin)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TelephoneNumber = telephoneNumber;
+            AdressesId = adressesId;
+            LoginsId = loginsId;
+            IsAdmin = isAdmin;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}, {TelephoneNumber}";
+        }
+    }
+}
