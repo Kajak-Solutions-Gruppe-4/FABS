113497c3910d597d29f7d4d1371c2314b8033a4a
Smileyrasmus-UCN <76094701+Smileyrasmus-UCN@users.noreply.github.com>
2021-11-10 20:02:33 +0100

    Another temporary commit

--- FABS_Service/FABS_DataAccess/DataModel/FABSContext.cs
+++ FABS_Service/FABS_DataAccess/DataModel/FABSContext.cs
@@ -5,26 +5,26 @@ using System.Text;
 
 namespace FABS_DataAccess.Model
 {
-    public partial class FABSContext
-    {
-        public FABSContext()
-        {
-            string appSettingsString = @"\FABS_API_Service\appsettings.json";
-            // Loops to search back in folders to be able to find appsettings.
-            // TODO: find a better solution than just looping 10 times
-            for (int i = 0; i < 10; i++)
-            {
-                string physicalPath = @Directory.GetCurrentDirectory() + appSettingsString;
-                if (physicalPath.Contains(@"FABS_Service\FABS_API_Service\appsettings.json"))
-                {
-                    break;
-                }
-            }
+    //public partial class FABSContext
+    //{
+    //    public FABSContext()
+    //    {
+    //        string appSettingsString = @"\FABS_API_Service\appsettings.json";
+    //        // Loops to search back in folders to be able to find appsettings.
+    //        // TODO: find a better solution than just looping 10 times
+    //        for (int i = 0; i < 10; i++)
+    //        {
+    //            string physicalPath = @Directory.GetCurrentDirectory() + appSettingsString;
+    //            if (physicalPath.Contains(@"FABS_Service\FABS_API_Service\appsettings.json"))
+    //            {
+    //                break;
+    //            }
+    //        }
 
-            Configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile(@Directory.GetCurrentDirectory() + @"\..\..\..\..\FABS_API_Service\appsettings.json")
-                .Build();
-        }
-    }
+    //        Configuration = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile(@Directory.GetCurrentDirectory() + @"\..\..\..\..\FABS_API_Service\appsettings.json")
+    //            .Build();
+    //    }
+    //}
 }
