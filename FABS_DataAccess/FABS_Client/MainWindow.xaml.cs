92e3a75089036b0de5813764f8f644445680ffc2
Lars Andersen <larskjandersen@gmail.com>
2021-11-10 11:27:37 +0100

    WPF testing

---
+++ FABS_DataAccess/FABS_Client/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FABS_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            var newForm = new Pages.PersonsGui();
+        }
+
+        private void ButtonPerson_Click(object sender, RoutedEventArgs e)
+        {
+            //Main.Content = new Pages.PersonsGUI();
+        }
+    }
+}
