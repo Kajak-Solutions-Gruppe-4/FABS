9493f5e29a1da659e144a85ad931025f137e8262
Smileyrasmus-UCN <1086243@ucn.dk>
2021-11-15 10:57:59 +0100

    Fix bug (butch)

--- FABS_Service/FABS_API_Service/Startup.cs
+++ FABS_Service/FABS_API_Service/Startup.cs
@@ -26,6 +26,9 @@ namespace FABS_API_Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // TODO: should not need to ingore
+            services.AddControllers().AddNewtonsoftJson(x =>
+                x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
