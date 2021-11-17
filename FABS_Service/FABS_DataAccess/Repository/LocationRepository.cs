19a59a7cab0b49df160b2d6716326492e5485b44
Allan <1062965@hotmail.com>
2021-11-17 09:09:35 +0100

    Update LocationRepository.cs
    Location repository update

--- FABS_Service/FABS_DataAccess/Repository/LocationRepository.cs
+++ FABS_Service/FABS_DataAccess/Repository/LocationRepository.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using FABS_DataAccess.Model;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace FABS_DataAccess.Repository
 {
-    class LocationRepository
+    public class LocationRepository : IRepository<Location>
     {
+        public int Create(Location t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Location Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Location> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, Location t)
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
