b2ab427e06185246d6bdc19b1934470cd98516fb
Smileyrasmus-UCN <1086243@ucn.dk>
2021-11-19 11:53:00 +0100

    Item things

---
+++ FABS_Service/FABS_DataAccess/DataModel/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FABS_DataAccess.Model
+{
+    public partial class Item
+    {
+        public Item(int organisationsId, int itemTypesId) : this()
+        {
+            OrganisationsId = organisationsId;
+            ItemTypesId = itemTypesId;
+        }
+
+        public Item(int organisationsId, ItemType itemTypes) : this()
+        {
+            OrganisationsId = organisationsId;
+            ItemTypes = itemTypes;
+        }
+
+        public Item(Organisation organisations, ItemType itemTypes) : this()
+        {
+            Organisations = organisations;
+            ItemTypes = itemTypes;
+        }
+
+        public Item(Organisation organisations, int itemTypesId) : this()
+        {
+            Organisations = organisations;
+            ItemTypesId = itemTypesId;
+        }
+    }
+}
---
+++ FABS_Service/FABS_DataAccess/DataModel/KayakType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FABS_DataAccess.Model
+{
+    public partial class KayakType
+    {
+        public KayakType()
+        {
+        }
+
+        public KayakType(string description, int weightLimit, decimal lengthMeter, int personCapacity, ItemType itemType) : this()
+        {
+            Description = description;
+            WeightLimit = weightLimit;
+            LengthMeter = lengthMeter;
+            PersonCapacity = personCapacity;
+            ItemType = itemType;
+        }
+    }
+}
