69e658827a31b1d3452cb6cba40bc686c6aad6f0
Smileyrasmus-UCN <1086243@ucn.dk>
2021-11-19 11:30:16 +0100

    Location datamodel

--- FABS_Service/FABS_DataAccess/DataModel/Location.cs
+++ FABS_Service/FABS_DataAccess/DataModel/Location.cs
@@ -6,10 +6,22 @@ namespace FABS_DataAccess.Model
 {
     public partial class Location
     {
-        public Location(string pickLocation, string description)
+        public Location(string pickLocation, string description) : this()
         {
             PickLocation = pickLocation;
             Description = description;
+        }
+
+        public Location(string pickLocation, int organisationsId) : this()
+        {
+            PickLocation = pickLocation;
+            OrganisationsId = organisationsId;
+        }
+
+        public Location(string pickLocation, Organisation organisations) : this()
+        {
+            PickLocation = pickLocation;
+            Organisations = organisations;
         }
     }
 }
