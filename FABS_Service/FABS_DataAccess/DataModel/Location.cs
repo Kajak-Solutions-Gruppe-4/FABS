c9349b3b4b149df9500cd7dacfcb84b186096356
Smileyrasmus-UCN <1086243@ucn.dk>
2021-11-19 10:21:31 +0100

    removed error

--- FABS_Service/FABS_DataAccess/DataModel/Location.cs
+++ FABS_Service/FABS_DataAccess/DataModel/Location.cs
@@ -6,10 +6,9 @@ namespace FABS_DataAccess.Model
 {
     public partial class Location
     {
-        public Location(string pickLocation, bool isInUse, string description)
+        public Location(string pickLocation, string description)
         {
             PickLocation = pickLocation;
-            IsInUse = isInUse;
             Description = description;
         }
     }
