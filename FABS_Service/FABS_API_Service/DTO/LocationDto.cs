c19051dc8bdaca1f0d472fb3086c3607b39047d2
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2021-11-24 09:24:43 +0100

    Oprettet DTO'er
    Oprettet DTO'er og forsøg på at lave en convert. Kan jeg få en til at assistere mig med en gennemgang? Jeg er usikker på om jeg har gjort det rigtigt eller med de rigtige informationer
    @Maibritt

---
+++ FABS_Service/FABS_API_Service/DTO/LocationDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FABS_API_Service.DTO
+{
+    public class LocationDto
+    {
+        public int Id { get; set; }
+        public string PickLocation { get; set; }
+        public string Description { get; set; }
+        public WarehouseDto Warehouse { get; set; }
+        public PersonDto People { get; set; }
+        public OrganisationDto Organisation { get; set; }
+
+        public LocationDto()
+        {
+
+        }
+
+        public LocationDto(int id, string pickLocation, string description, WarehouseDto warehouseDto, PersonDto peopleDto, OrganisationDto organisationDto)
+        {
+            Id = id;
+            PickLocation = pickLocation;
+            Description = description;
+            Warehouse = warehouseDto;
+            People = peopleDto;
+            Organisation = organisationDto;
+
+        }
+
+        public LocationDto(string pickLocation, string description, WarehouseDto warehouseDto, PersonDto peopleDto, OrganisationDto organisationDto)
+        {
+            PickLocation = pickLocation;
+            Description = description;
+            Warehouse = warehouseDto;
+            People = peopleDto;
+            Organisation = organisationDto;
+        }
+    }
+}
